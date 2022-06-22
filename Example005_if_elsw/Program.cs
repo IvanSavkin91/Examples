a9360a9f76949a476e7b9dc474524aa6f7a7712c
IvanSavkin <adoys1@gmail.com>
2022-06-22 15:53:47 +0300

    if_else

---
+++ Example005_if_elsw/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ваня")
+{
+    Console.WriteLine("Ура, босс Ваня");
+}
+    else
+    {
+        Console.Write("Привет, ");
+        Console.WriteLine(username);
+    }
